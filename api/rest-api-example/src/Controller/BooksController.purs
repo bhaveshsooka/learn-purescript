f10ffeb85713d3f6efe38acb740c767a5928f2e1
Bhavesh <bhavesh@synthesis.co.za>
2021-04-03 22:46:48 +0200

    Converted path params to query params + reordered requests

--- api/rest-api-example/src/Controller/BooksController.purs
+++ api/rest-api-example/src/Controller/BooksController.purs
@@ -5,34 +5,33 @@ import Books
 import Data.Either (Either(..))
 import Data.Int (fromString)
 import Data.Maybe (Maybe(..))
-import Data.String (length)
 import Data.Time.Duration (Milliseconds(..))
 import Effect.Aff (delay)
-import HTTPure (Method(..), Request, ResponseM, badRequest, internalServerError, ok, (!@))
-import Prelude (discard, otherwise, show, ($), (&&), (<>), (==), (>))
+import HTTPure (Method(..), Request, ResponseM, badRequest, notImplemented, ok, (!?), (!@))
+import Prelude (discard, otherwise, show, ($), (&&), (<>), (==))
 import Simple.JSON (readJSON)
 
 -- | We can safely assume that the path is '/api/books*'
 
 routerBook :: Request -> ResponseM
-routerBook req
-  | req.method == Get && (length $ req.path !@ 2) > 0
-    = getBook $ req.path !@ 2
+routerBook req    
+  | req.method == Get && req.query !? "id"
+    = getBook $ req.query !@ "id"
+
+  | req.method == Put && req.query !? "id"
+    = updateBook $ req.query !@ "id"
+  
+  | req.method == Delete && req.query !? "id"
+    = deleteBook $ req.query !@ "id"
   
   | req.method == Get
     = getBooks req
 
   | req.method == Post
     = addBook req.body
-  
-  | req.method == Put
-    = updateBook $ req.path !@ 2
-  
-  | req.method == Delete
-    = deleteBook $ req.path !@ 2
-  
+
   | otherwise
-    = internalServerError "Method not implemented"
+    = notImplemented
 
 getBooks :: Request -> ResponseM
 getBooks req = do
